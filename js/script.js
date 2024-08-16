6ecbd21fa38e7ac35a10e0111b1b3a11d792b448
Deep-Gohil <deep72617626@gmail.com>
2024-08-16 17:30:23 +0530

    portfolio

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ var typed = new Typed('#element', {
     strings:['Web Designer','Web Developer'],
     typeSpeed: 100,
 });
+
+let h1 = document.createElement('h1');
+document.getElementById("h1").innerHTML = "This Site is not available For mobil..."
