ae96fd5ed64fa4239c027f1f1b61deb34dd09400
Deep-Gohil <deep72617626@gmail.com>
2024-08-18 13:31:00 +0530

    Portfolio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 var typed = new Typed('#element', {
-    // strings: ['', 'Web Developer'],
     strings:['Web Designer','Web Developer'],
     typeSpeed: 100,
 });
 
-let h1 = document.createElement('h1');
-document.getElementById("h1").innerHTML = "This Site is not available For mobil..."
+let tabLinks = document.querySelectorAll(".tabLinks");
+let tabContents = document.querySelectorAll(".tabContent");
+
+const opentab = (tabname) => {
+    tabLinks.forEach((link) => {
+        link.classList.remove("activeLinks");
+    });
+
+    tabContents.forEach((content) => {
+        content.classList.remove("activeTab");
+    });
+
+    document.querySelector(`.tabLinks[onclick="opentab('${tabname}')"]`).classList.add("activeLinks");
+    document.getElementById(tabname).classList.add("activeTab");
+};
