30f2efa4699d95af5f494ba5e3dbc90d9381d84e
Deep-Gohil <deep72617626@gmail.com>
2024-09-05 16:49:51 +0530

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#element', {
-    strings:['Web Designer','Web Developer'],
+    strings: ['Web Designer', 'Web Developer'],
     typeSpeed: 100,
 });
 
@@ -20,16 +20,39 @@ const opentab = (tabname) => {
 };
 
 let sidemenu = document.getElementById("sidemenu");
-    
-    const handleOpenMenu = () => {
-        sidemenu.style.right = "0"
+
+const handleOpenMenu = () => {
+    sidemenu.style.right = "0"
+}
+
+const handleCloseMenu = () => {
+    sidemenu.style.right = "-200px"
+}
+
+document.getElementById("openmenu").addEventListener("click", handleOpenMenu)
+document.getElementById("closemenu").addEventListener("click", handleCloseMenu)
+
+
+function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
+
+// copied from chat GPT 
+document.getElementById('mobile-number').addEventListener('click', function () {
+    const mobileNumber = this.innerText;
+    if (isMobileDevice()) {
+        window.location.href = `tel:${mobileNumber}`;
+    } else {
+        navigator.clipboard.writeText(mobileNumber)
+            .then(() => {
+                alert('Mobile number copied to clipboard!');
+            })
+            .catch(err => {
+                console.error('Failed to copy number: ', err);
+            });
     }
-
-    const handleCloseMenu = () => {
-        sidemenu.style.right = "-200px"
-    }
-
-    document.getElementById("openmenu").addEventListener("click", handleOpenMenu)
-    document.getElementById("closemenu").addEventListener("click", handleCloseMenu)
-
-    
+});
+document.getElementById('email').addEventListener('click', function () {
+    const email = this.innerText;
+    window.location.href = `mailto:${email}`;
+});
